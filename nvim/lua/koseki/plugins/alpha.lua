1b25ba5a74884b013d1963b83771a105e97b0e39
bordon.leandro <leandro.bordon@balloon-group.com>
2024-07-07 00:03:19 -0300

    :sparkles: new custom dasboard

--- nvim/lua/koseki/plugins/alpha.lua
+++ nvim/lua/koseki/plugins/alpha.lua
@@ -7,24 +7,22 @@ return {
 
     -- Set header
     dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
+"                                   ",
+"▄▄▄▄·       ▄▄▄  ·▄▄▄▄         ▐ ▄ ",
+"▐█ ▀█▪▪     ▀▄ █·██▪ ██ ▪     •█▌▐█",
+"▐█▀▀█▄ ▄█▀▄ ▐▀▀▄ ▐█· ▐█▌ ▄█▀▄ ▐█▐▐▌",
+"██▄▪▐█▐█▌.▐▌▐█•█▌██. ██ ▐█▌.▐▌██▐█▌",
+"·▀▀▀▀  ▀█▄▀▪.▀  ▀▀▀▀▀▀•  ▀█▄▀▪▀▀ █▪",
     }
 
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+      dashboard.button("e", "📄 > New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "🗂️ > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "🔎 > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "🔍 > Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC wr", "↩️  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", "✖️  > Quit NVIM", "<cmd>qa<CR>"),
     }
 
     -- Send config to alpha
